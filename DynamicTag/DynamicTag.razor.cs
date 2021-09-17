91da4d366dc874653c231ac86bc94b38140a1d80
Santiago Cattaneo <santiago@rd-its.com>
2021-09-17 20:34:06 -0300

    Add project files.

---
+++ DynamicTag/DynamicTag.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace DynamicTag
+{
+    public class DynamicTag : ComponentBase
+    {
+        [Parameter]
+        public string Tag { get; set; } = "div";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+            var ix = 0;
+            __builder.OpenElement(ix++, Tag);
+            if (ChildContent != null)
+            {
+                __builder.AddContent(ix++, ChildContent);
+            }
+            __builder.CloseElement();
+        }
+    }
+}
